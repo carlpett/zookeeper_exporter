887768eace7215e376c1b3a5abfa6909e9dda398
Calle Pettersson <cpettsson@gmail.com>
2016-05-02 09:33:18 +0200

    Add root handler on server

--- metrics-server.go
+++ metrics-server.go
@@ -6,6 +6,17 @@ import (
   log "github.com/Sirupsen/logrus"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte(`<html>
+      <head><title>Zookeeper Exporter</title></head>
+      <body>
+      <h1>Zookeeper Exporter</h1>
+      <p><a href="` + *metricsPath + `">Metrics</a></p>
+      </body>
+      </html>`))
+}
+
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
   log.WithFields(log.Fields { "request": r }).Debug("Incoming request to /metrics")
 
@@ -13,6 +24,7 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
     log.Error("Call to /metrics before successfully collecting metrics from Zookeeper!")
 
     w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Internal server error"))
     return
   }
 
@@ -23,5 +35,6 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
 func serveMetrics() {
   log.Info("Starting metric http endpoint on :8090")
   http.HandleFunc("/metrics", metricsHandler)
-  log.Fatal(http.ListenAndServe(":8090", nil))  
+  http.HandleFunc("/", rootHandler)
+  log.Fatal(http.ListenAndServe(":8090", nil))
 }
